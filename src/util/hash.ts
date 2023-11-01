71cc0b5e59addc7888fe655f766e5b66b575ed3c
Giancarlo Fontela da Luz <giancarlo.luz@cloud2go.com.br>
2023-11-01 17:02:39 -0300

    fix: Hashing function creation

--- src/util/hash.ts
+++ src/util/hash.ts
@@ -1,7 +1,7 @@
 import { createHash } from 'crypto';
 
-const hashingFunction = createHash('md5');
+const HASHING_FUNCTION = 'md5';
 
 export default function hash(value: string): string {
-    return hashingFunction.update(value).digest('base64');
+    return createHash(HASHING_FUNCTION).update(value).digest('base64');
 }
